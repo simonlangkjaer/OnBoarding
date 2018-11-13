1c927b9bc4ac48eae54a11d0126ae713baca2245
Simon Langkjaer <simon.langkjaer@virtual-identity.com>
2018-11-13 15:53:42 +0100

    Navbar using jquery and grid

--- js/main.js
+++ js/main.js
@@ -2,13 +2,18 @@
   window.onscroll = function() {scrollFunction()};
 
   function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      document.getElementById("headershrink").style.height = "30px"; 
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      // document.querySelector(".headershrink").style.height = "30px"; 
+      // console.log(document.querySelectorAll(".headershrink")[0]);
       $('.wrapsearch').removeClass('show'); 
       $('.linknav:link').css('font-size','16px');
+      $('.headershrink').addClass('showheadershrink'); 
+   
+    
     } else {
-      document.getElementById("headershrink").style.height = "67px";
+    //  document.getElementById("headershrink").style.height = "67px";
       $('.wrapsearch').addClass('show');
       $('.linknav:link').css('font-size','18px');
+      $('.headershrink').removeClass('showheadershrink');
     }
   } 
