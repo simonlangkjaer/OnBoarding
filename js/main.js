9c7da4ae01cadc00da7d6dc7b6f0c65e2a9977b1
Simon Langkjaer <simon.langkjaer@virtual-identity.com>
2018-11-14 14:54:12 +0100

    Slider with dots and next, prev.

--- js/main.js
+++ js/main.js
@@ -5,15 +5,45 @@
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
       // document.querySelector(".headershrink").style.height = "30px"; 
       // console.log(document.querySelectorAll(".headershrink")[0]);
-      $('.wrapsearch').removeClass('show'); 
-      $('.linknav:link').css('font-size','16px');
-      $('.headershrink').addClass('showheadershrink'); 
-   
-    
+      $('.headershrink').addClass('showheadershrink');
+      $('.searchparent').addClass('searchhide');
+      $('.linknav').addClass('fontchange');
+
     } else {
     //  document.getElementById("headershrink").style.height = "67px";
-      $('.wrapsearch').addClass('show');
-      $('.linknav:link').css('font-size','18px');
-      $('.headershrink').removeClass('showheadershrink');
+    $('.headershrink').removeClass('showheadershrink');  
+    $('.searchparent').removeClass('searchhide');
+    $('.linknav').removeClass('fontchange');
     }
   } 
+
+// function 
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
